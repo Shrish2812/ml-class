3220c220198d43dd1fe3687add7d9459b7ffb0b3
Shrish <shrish.nagvekar@gmail.com>
2019-03-27 18:22:41 +0000

    Single perceptron

--- keras-perceptron/.ipynb_checkpoints/perceptron-linear-checkpoint.py
+++ keras-perceptron/.ipynb_checkpoints/perceptron-linear-checkpoint.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
+from keras.utils import to_categorical
 
 import wandb
 from wandb.keras import WandbCallback
@@ -11,22 +12,24 @@ config = run.config
 
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-is_five_train = y_train == 5
-is_five_test = y_test == 5
-labels = ["Not Five", "Is Five"]
-
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
+
+# one hot encode outputs
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+labels = range(10)
+
+num_classes = y_train.shape[1]
 
 # create model
 model = Sequential()
 model.add(Flatten(input_shape=(img_width, img_height)))
-model.add(Dense(1))
+model.add(Dense(num_classes))
 model.compile(loss='mse', optimizer='adam',
               metrics=['accuracy'])
 
 # Fit the model
-model.fit(X_train, is_five_train, epochs=3, batch_size=64, validation_data=(X_test, is_five_test),
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
           callbacks=[WandbCallback(data_type="image", labels=labels, save_model=False)])
-model.save('perceptron.h5')
+model.save('model.h5')
--- keras-perceptron/.ipynb_checkpoints/perceptron-single-fixed-checkpoint.py
+++ keras-perceptron/.ipynb_checkpoints/perceptron-single-fixed-checkpoint.py
@@ -12,6 +12,7 @@ config = run.config
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+# create labels
 is_five_train = y_train == 5
 is_five_test = y_test == 5
 labels = ["Not Five", "Is Five"]
@@ -22,11 +23,11 @@ img_height = X_train.shape[2]
 # create model
 model = Sequential()
 model.add(Flatten(input_shape=(img_width, img_height)))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam',
-              metrics=['accuracy'])
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam',
+              metrics=['binary_accuracy'])
 
 # Fit the model
-model.fit(X_train, is_five_train, epochs=3, batch_size=64, validation_data=(X_test, is_five_test),
+model.fit(X_train, is_five_train, epochs=3, validation_data=(X_test, is_five_test),
           callbacks=[WandbCallback(data_type="image", labels=labels, save_model=False)])
 model.save('perceptron.h5')
